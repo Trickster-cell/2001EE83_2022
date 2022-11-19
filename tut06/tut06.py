f9a6894c484929113486561b7fb5e6e3e1947c64
Yash Raj <yash_2001ee83@iitp.ac.in>
2022-11-19 14:46:39 +0530

    did something more
    email part remaining

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,162 +1,225 @@
-from calendar import week
-import datetime
-from re import A
-from time import strftime
+import openpyxl
+from openpyxl import load_workbook
 import pandas as pd
 import os
-import csv
+import datetime
 
-def RollSplit(att):
-    roll = ""
-    for i in range(0,8):
-        roll+=att[i]
-    return roll
+head4indi = ["Date", "Roll", "Name", "Total Attendance Count",
+             "Real", "Duplicate", "Invalid", "Absent"]
 
-def NameSplit(att):
-    name = ""
-    for i in range(9,len(att)):
-        name+=att[i]
-    return name
 
-def MonOrThurs(weekday):
-    
-    if (weekday == "Monday"):
+def setheadindi(wb):
+    for i in range(len(head4indi)):
+        wb.cell(row=1, column=1+i).value = head4indi[i]
+
+
+def firstrowset(wb, validunique):
+    for i in range(len(validunique)):
+        wb.cell(row=3+i, column=1).value = validunique[i]
+
+
+def validtime(timetem):
+    if timetem == "15:00":
         return True
-    
-    if (weekday == "Thursday"):
-        return True
-    
-    return False
-
-def bet2n3(timeMarked):
-    if(timeMarked[0]=='1' and timeMarked[1]=='4'):
-        return True
-    if(timeMarked=="15:00:00"):
+    hr = timetem[0:2]
+    if hr == "14":
         return True
     return False
 
-def splitDate(timestr):
-    finstr = ""
-    for i in range(0,10):
-        finstr+=timestr[i]
-    
-    return finstr
 
-def splitTime(timestr):
-    finstr = ""
-    for i in range(11,len(timestr)):
-        finstr+=timestr[i]
-    
-    return finstr
+def rollsplit(rollpname):
+    ans = ""
+    for i in range(8):
+        ans += rollpname[i]
+    return ans
 
 
-def checkDate(timestr, alldates):
-    Date = splitDate(timestr)
-    TimeMarked = splitTime(timestr)
-    # length = len(timestr)
-    # for i in range(0,10):
-    #     Date+=timestr[i]
-    # for i in range(11, length):
-    #     TimeMarked+=(str)(timestr[i])
-    
-    day, month, year = (int(x) for x in Date.split('/'))
-    ans = datetime.date(year, month, day)
-    print(ans.strftime("%A"))
-    # alldates = set(alldates)
-    weekday = str(ans.strftime("%A"))
-    if (MonOrThurs(weekday)):
-        alldates.append(Date)
-        return True
-        # if(bet2n3(TimeMarked)): 
-            # print("ok")
-    # print(TimeMarked)
-    # print(Date)
-    return False    
+def namesplit(rollpname):
+    ans = rollpname[9:]
+    return ans
+
+
+def datesplit(timeofatt):
+    ans = timeofatt[0:10]
+    return ans
+
+
+def timesplit(timeofatt):
+    ans = timeofatt[11:]
+    return ans
+
+
+def per(a, b):
+    c = a/b
+    c *= 100
+    return c
 
 
 registered = pd.read_csv('input_registered_students.csv')
-# registered.reset_index()
-students = []
+
+dictofatt = {}
+dictofnames = {}
+dictoffakes = {}
+regroll = []
+dictofRoll = {}
+
+os.makedirs('OutputTemp', exist_ok=True)
+
+cons = openpyxl.Workbook()
+consolidated = cons.active
+
+consolidated.cell(row=1, column=1).value = "Roll"
+consolidated.cell(row=1, column=2).value = "Name"
+
 
 for i in range(len(registered)):
-    # print(registered.iloc[i, 0], registered.iloc[i, 1])
-    strin = (str)(registered.iloc[i,0]) + " " + (str)(registered.iloc[i,1])
-    students.append(strin)
-    
+    tempr = (str)(registered.iloc[i, 0])
+    tempn = (str)(registered.iloc[i, 1])
+    dictofnames[tempr] = tempn
+    regroll.append(tempr)
+    dictofatt[tempr] = []
+    rolltemp = openpyxl.Workbook()
+    rolltempfile = rolltemp.active
+    path = "OutputTemp/" + (str)(tempr) + ".xlsx"
+    rolltemp.save(path)
 
-# print(students)
-fieldnames = ["Roll","Name","total_lecture_taken","attendance_count_actual","attendance_count_fake","attendance_count_absent","Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal" ]
 
-att_sheet = pd.read_csv('input_attendance.csv')
+attendance = pd.read_csv('input_attendance.csv')
 
-times = []
-for i in range(len(att_sheet)):
-    times.append((str)(att_sheet.iloc[i,0]))
-    
-diffDates = []
-for time in times:
-    # print(time)
-    checkDate(time, diffDates)
+validdates = []
 
-diffDates = set(diffDates)
-# print(diffDates)
-totalLectures = len(diffDates)
-# print(totalLectures)
-# print(times)
+for i in range(len(attendance)):
+    datetemp = datesplit(attendance.iloc[i, 0])
+    day = pd.to_datetime(datetemp, format="%d-%m-%Y")
+    if (day.day_name() == "Monday" or day.day_name() == "Thursday"):
+        validdates.append(datetemp)
 
-templist = []
+validdates = set(validdates)
 
-os.makedirs('Outputtemp', exist_ok=True)
-fprime = open("Outputtemp/attendance_report_consolidated.csv", "w", newline='')
-fdupli = open("Outputtemp/attendance_report_duplicate.csv", "w", newline='')
-writerprime= csv.writer(fprime)
-writerprime.writerow(fieldnames)
-duplifield = ["Timestamp","Roll","Name","Total count of attendance on that day"]
-writerdupli = csv.writer(fdupli)
-writerdupli.writerow(duplifield)
-for student in students:
-    # print(student)ṇ
-    indivatt = att_sheet[att_sheet['Attendance']==student]
-    
-    tot_marked = (len(indivatt))
-    fake_marked = 0
-    dupli_marked = 0
-    dicttemp = {}
-    name = NameSplit(student)
-    roll = RollSplit(student)
-    # print(indivatt)
-    for i in range(len(indivatt)):
-        timestr = (str)(indivatt.iloc[i,0])
-        datetemp = splitDate(timestr)
-        timetemp = splitTime(timestr)
-        sztemp = len(dicttemp)
-        # print(timetemp)
-        if (bet2n3(timetemp)==False):
-            fake_marked+=1
-        elif(checkDate(timestr, templist) == False):
-            fake_marked+=1
+sortedValidDates = []
+for i in validdates:
+    sortedValidDates.append(i)
+
+
+sortedValidDates.sort(
+    key=lambda date: datetime.datetime.strptime(date, "%d-%m-%Y"))
+# print(sortedValidDates)
+
+totalLectures = (len)(validdates)
+
+x = 0
+for i in regroll:
+    dictofRoll[i] = x+1
+    consolidated.cell(row=2+x, column=1).value = i
+    consolidated.cell(row=2+x, column=2).value = dictofnames[i]
+    x += 1
+
+head2 = ["Actual Lecture Taken", "Total Real", "% Attendance"]
+
+for i in range(len(head2)):
+    consolidated.cell(row=1, column=3+totalLectures+i).value = head2[i]
+
+for i in range(len(attendance)):
+
+    rollpname = attendance.iloc[i, 1]
+    rollt = rollsplit(rollpname)
+    namet = namesplit(rollpname)
+    rolltemp = openpyxl.Workbook()
+    rolltempfile = rolltemp.active
+    setheadindi(rolltempfile)
+    firstrowset(rolltempfile, sortedValidDates)
+    rolltempfile.cell(row=2, column=2).value = rollt
+    rolltempfile.cell(row=2, column=3).value = namet
+    path = "OutputTemp/" + (str)(rollt) + ".xlsx"
+
+    timeofatt = attendance.iloc[i, 0]
+    timetemp = timesplit(timeofatt)
+    datetemp = datesplit(timeofatt)
+    day = pd.to_datetime(datetemp, format="%d-%m-%Y")
+    rolltemp.save(path)
+
+    dictofatt[rollt].append(timeofatt)
+
+
+# print(dictofatt)
+
+
+# dicttemp = {}
+# for i in validdates:
+#     dicttemp[i] = 0
+
+# # print(dicttemp)
+
+for i in regroll:
+    timestamplist = dictofatt[i]
+    timestamplist = sorted(
+        timestamplist, key=lambda date: datetime.datetime.strptime(date, "%d-%m-%Y %H:%M"))
+
+    totalmarked = {}
+    validmarked = {}
+    duplimarked = {}
+    invalidmarked = 0
+    for t in validdates:
+        totalmarked[t] = 0
+        validmarked[t] = 0
+        duplimarked[t] = 0
+
+    # print(totalmarked, validmarked, duplimarked)
+    for times in timestamplist:
+        dateof = datesplit(times)
+        timeof = timesplit(times)
+
+        if dateof in validdates:
+            totalmarked[dateof] += 1
+            if validtime(timeof):
+                if validmarked[dateof] == 0:
+                    validmarked[dateof] += 1
+                else:
+                    duplimarked[dateof] += 1
+                    # print(totalmarked[dateof], validmarked[dateof], duplimarked[dateof])
         else:
-            if(datetemp in dicttemp):
-                dicttemp[datetemp]+=1
-            else:
-                dicttemp[datetemp]=1
-            if(len(dicttemp)==sztemp):
-                dupli_marked+=1
-                writerdupli = csv.DictWriter(fdupli, fieldnames=duplifield)
-                writerdupli.writerow({'Timestamp':timetemp, 'Roll':roll,'Name':name,'Total count of attendance on that day':dupli_marked})
+            invalidmarked += 1
 
-                
-    # print(tot_marked, fake_marked, dupli_marked)
-    actual = tot_marked-fake_marked-dupli_marked
-    fake = fake_marked+dupli_marked
-    percentage = (actual*100)/totalLectures
-    percentage = str(round(percentage, 2))
-    filename = "Outputtemp/" + (str)(roll) +".csv"
-    f = open(filename, "w", newline='')
-    writer = csv.writer(f)
-    writer.writerow(fieldnames)
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writerow({'Roll':roll, 'Name':name, 'total_lecture_taken':totalLectures, 'attendance_count_actual':actual, 'attendance_count_fake':fake, 'attendance_count_absent':totalLectures-actual, 'Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal':percentage})
-    writerprime2 = csv.DictWriter(fprime, fieldnames=fieldnames)
-    writerprime2.writerow({'Roll':roll, 'Name':name, 'total_lecture_taken':totalLectures, 'attendance_count_actual':actual, 'attendance_count_fake':fake, 'attendance_count_absent':totalLectures-actual, 'Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal':percentage})
-    
+    path = "OutputTemp/" + (str)(i) + ".xlsx"
+    # filename = (str)(i) + ".xlsx"
+    tempwb = load_workbook(path)
+    tempsheet = tempwb.active
+
+    # print(duplimarked[dateofroll])
+    # print(totalmarked["2001EE83"])
+
+    x = 0
+    for j in validdates:
+        tempsheet.cell(row=3+x, column=4).value = totalmarked[j]
+        tempsheet.cell(row=3+x, column=5).value = validmarked[j]
+        tempsheet.cell(row=3+x, column=6).value = duplimarked[j]
+        tempsheet.cell(
+            row=3+x, column=7).value = totalmarked[j]-validmarked[j]-duplimarked[j]
+        if validmarked[j] > 0:
+            tempsheet.cell(row=3+x, column=8).value = "YES"
+            consolidated.cell(row=1+dictofRoll[i], column=3+x).value = "P"
+        else:
+            tempsheet.cell(row=3+x, column=8).value = "NO"
+            consolidated.cell(row=1+dictofRoll[i], column=3+x).value = "A"
+        x += 1
+
+    tempwb.save(path)
+
+# print(dictofatt)
+
+for i in range(len(dictofRoll)):
+    x = 0
+    for j in range(totalLectures):
+        val = consolidated.cell(row=2+i, column=3+j).value
+        if val == "P":
+            x += 1
+    consolidated.cell(row=2+i, column=3+totalLectures).value = totalLectures
+    consolidated.cell(row=2+i, column=4+totalLectures).value = x
+    consolidated.cell(row=2+i, column=5 +
+                      totalLectures).value = per(x, totalLectures)
+
+
+for i in range(len(sortedValidDates)):
+    consolidated.cell(row=1, column=3+i).value = sortedValidDates[i]
+
+cons.save("OutputTemp/input_attendance_consolidated.xlsx")
